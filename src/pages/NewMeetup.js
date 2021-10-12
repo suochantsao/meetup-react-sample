059a762154bd199653ea1b954dd8ad207b439a5f
suochantsao <suochantsao@gmail.com>
2021-10-13 01:09:51 +0800

    Form added / Firebase API settings

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,26 @@
+// Components
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-example-7f0c9-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
